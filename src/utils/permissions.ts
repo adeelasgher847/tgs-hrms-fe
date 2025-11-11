83162c27408e744999b9e4638b643dc4f396b834
kishwarbukhar87 <kishwarbukhar87@gmail.com>
2025-11-11 22:06:48 +0500

    updated tenant list

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -263,6 +263,11 @@ export const isSubMenuVisibleForRole = (
     if (parent.includes('audit logs')) {
       visible = false;
     }
+    if (parent.includes('attendance')) {
+      if (sub.includes('report')) {
+        visible = false;
+      }
+    }
   }
 
   if (r === 'employee' || r === 'user') {
@@ -376,6 +381,14 @@ export const isSubMenuVisibleForRole = (
   if (r === 'manager') {
     if (parent.includes('assets')) {
       if (sub.includes('asset inventory') || sub.includes('management')) {
+        visible = false;
+      }
+    }
+     if (
+      parent.includes('leave analytics') ||
+      parent.includes('leave-analytics')
+    ) {
+      if (sub.includes('cross tenant leaves')) {
         visible = false;
       }
     }
